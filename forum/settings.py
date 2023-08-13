0e2cb0ca4fec86d68d2c2407d70b4f5b73eb4d34
Lucas Araujo <lucasasr25@gmail.com>
2023-08-13 18:44:53 -0300

    vecel commit

--- forum/settings.py
+++ forum/settings.py
@@ -15,6 +15,7 @@ from decouple import Config, Csv
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+config = Config()
 
 
 # Quick-start development settings - unsuitable for production
